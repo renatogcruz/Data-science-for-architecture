3fd973a8a25a3b349db6b9bc3b343f3ee64697b6
renatogcruz <renatogcruz@hotmail.com>
2021-03-14 10:29:58 -0300

    ifcopenshell - basics wwitch notebooks

---
+++ ifc_analysis/IFC_analysis_with_IfcOpenShell/03_Basics_with_ifcopenshell_notebook/README.md
@@ -0,0 +1,25 @@
+# Introduction to Python 3 in jupyter notebook
+
+This is an introduction to the Python programming language for Programming with openBIM.
+
+This tutorial will walk through:
+
+- Hello jupyter, world and you
+- Variables and types
+- Basic String formatting
+- Collection types (Arrays))
+- Conditions and If statements
+- While Loops
+- For Loops
+- Functions
+- Classes and Objects
+- Modules and Packages
+
+in aplication openBIM programming.
+
+At the end of this notebook you will learn how to visualize objects in a BIM model. Like the windows of Grethes hus
+
+
+**Download the files above and do it yourself.**
+
+Reference by BIMFag
---
+++ ifc_analysis/IFC_analysis_with_IfcOpenShell/03_Basics_with_ifcopenshell_notebook/ifc_viewer.py
@@ -0,0 +1,23 @@
+from OCC.Display.WebGl.jupyter_renderer import JupyterRenderer as Renderer
+
+class ifc_viewer(Renderer):
+    def __init__(self):
+        Renderer.__init__(self)
+        self.n = 0
+        self.products = []
+
+    @staticmethod
+    def subshapes(shp):
+        import OCC.Core.TopoDS
+        it = OCC.Core.TopoDS.TopoDS_Iterator(shp)
+        while it.More():
+            yield it.Value()
+            it.Next()
+
+    def DisplayShape(self, product, shp, color):
+        for shp, sty in zip(self.subshapes(shp), color):
+            Renderer.DisplayShape(self, shp, shape_color=sty[0:3], render_edges=True)
+            self.products.append(product)
+
+    def onclick(self, item):
+        self.html.value = "Selected %r" % self.products[int(item.name)]
